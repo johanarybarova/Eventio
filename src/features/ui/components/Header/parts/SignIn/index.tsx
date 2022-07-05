24ffe6a324f3d1d5c1f1fbc431eba679a3ecd216
Johana <johanarybarova@gmail.com>
2022-07-05 10:26:11 +0200

    feat: add Header ui

---
+++ src/features/ui/components/Header/parts/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import type { FC } from 'react'
+
+import { Routes } from '~/features/core/constants/routes'
+
+export const SignIn: FC = () => (
+  <Link href={Routes.LOGIN}>
+    <a href=" ">
+      Already have an account? <b>sign in</b>
+    </a>
+  </Link>
+)
