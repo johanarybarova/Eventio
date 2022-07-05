da3be9f56dcd4c2bf1f09eabdf825338465eff1b
Johana <johanarybarova@gmail.com>
2022-07-05 11:10:57 +0200

    refactor: update create event page with LayoutIn and LayoutEx

--- src/features/events/pages/CreateEventPage/index.tsx
+++ src/features/events/pages/CreateEventPage/index.tsx
@@ -1,7 +1,31 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
+import type { FormEvent } from 'react'
 
-export const CreateEventPage: NextPage = () => (
-  <main>
-    <h1>Create Event Page</h1>
-  </main>
-)
+import { Routes } from '~/features/core/constants/routes'
+import { Input } from '~/features/ui/components/Input'
+import { LayoutIn } from '~/features/ui/components/LayoutIn'
+
+export const CreateEventPage: NextPage = () => {
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    alert('TODO')
+  }
+
+  return (
+    <LayoutIn>
+      <Link href={Routes.DASHBOARD}>X Close</Link>
+      <h1>Create new event</h1>
+      <p>Enter details below.</p>
+      <form onSubmit={onSubmit}>
+        <Input label="Title" type="text" name="title" />
+        <Input label="Description" type="text" name="description" />
+        <Input label="Date" type="date" name="date" />
+        <Input label="Time" type="time" name="time" />
+        <Input label="Capacity" type="number" name="capacity" />
+        <button type="submit">Create New Event</button>
+      </form>
+    </LayoutIn>
+  )
+}
