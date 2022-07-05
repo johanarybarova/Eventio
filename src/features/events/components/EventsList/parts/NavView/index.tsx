1b3cc39fb52cae76796bc568f976ab5b99521565
Johana <johanarybarova@gmail.com>
2022-07-05 09:48:42 +0200

    feat: add NavView

---
+++ src/features/events/components/EventsList/parts/NavView/index.tsx
@@ -0,0 +1,33 @@
+import type { FC } from 'react'
+
+enum ViewType {
+  GRID = 'GRID',
+  LIST = 'LIST',
+}
+
+type Props = {
+  onChange: (viewType: ViewType) => void
+}
+
+export const NavView: FC<Props> = ({ onChange }) => (
+  <ul>
+    <li>
+      <button
+        type="button"
+        aria-label="Show as grid"
+        onClick={() => onChange(ViewType.GRID)}
+      >
+        #
+      </button>
+    </li>
+    <li>
+      <button
+        type="button"
+        aria-label="Show as list"
+        onClick={() => onChange(ViewType.LIST)}
+      >
+        =
+      </button>
+    </li>
+  </ul>
+)
