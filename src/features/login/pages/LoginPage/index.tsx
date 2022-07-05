fed1cee4f80213eb3af6c552786710a2976005b0
Johana <johanarybarova@gmail.com>
2022-07-05 11:12:51 +0200

    refactor: update login page with LayoutEx

--- src/features/login/pages/LoginPage/index.tsx
+++ src/features/login/pages/LoginPage/index.tsx
@@ -1,7 +1,25 @@
 import type { NextPage } from 'next'
+import type { FormEvent } from 'react'
 
-export const LoginPage: NextPage = () => (
-  <main>
-    <h1>Login Page</h1>
-  </main>
-)
+import { Input } from '~/features/ui/components/Input'
+import { LayoutEx } from '~/features/ui/components/LayoutEx'
+
+export const LoginPage: NextPage = () => {
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    alert('TODO')
+  }
+
+  return (
+    <LayoutEx>
+      <h1>Sign in to Eventio.</h1>
+      <p>Enter your details below.</p>
+      <form onSubmit={onSubmit}>
+        <Input label="Email" type="email" name="email" />
+        <Input label="Password" type="password" name="password" />
+        <button type="submit">Sign In</button>
+      </form>
+    </LayoutEx>
+  )
+}
