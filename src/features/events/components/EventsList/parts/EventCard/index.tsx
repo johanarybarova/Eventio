7fb3f61817780d2054d78444bef826cf68dc7b1d
Johana <johanarybarova@gmail.com>
2022-07-05 09:43:35 +0200

    feat: add EventCard

---
+++ src/features/events/components/EventsList/parts/EventCard/index.tsx
@@ -0,0 +1,14 @@
+import type { FC } from 'react'
+
+export const EventCard: FC = () => (
+  <article>
+    <time>July 4, 2022 – 5:20 PM</time>
+    <h3>How to stay calm when angry</h3>
+    <p>John Watts</p>
+    <p>I will show you how to stay calm when angry in a second</p>
+    <p>
+      5 <span>of</span> 15
+    </p>
+    <button type="button">Edit</button>
+  </article>
+)
