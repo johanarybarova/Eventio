4fbecc0e26f81f8165ab81512242bccbbcdce2a7
Johana <johanarybarova@gmail.com>
2022-07-13 14:28:29 +0200

    feat: add Styled CreateButton

---
+++ src/features/events/components/EventsList/parts/CreateButton/styled.ts
@@ -0,0 +1,23 @@
+import { styled } from '~/styles/stitches.config'
+
+export const CreateLink = styled('a', {
+  boxShadow: '0 6px 9px rgba(0, 0, 0, 0.15)',
+  zIndex: '900',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  position: 'fixed',
+  bottom: '0',
+  right: '0',
+  width: '5.6rem',
+  height: '5.6rem',
+  margin: '3.2rem',
+  borderRadius: '50%',
+  color: '$text$inverted',
+  cursor: 'pointer',
+  bg: '$background$dark',
+
+  fontSize: '2rem',
+  textDecoration: 'none',
+  paddingTop: '3px',
+})
