d53038f5d9875c3f42b8dce3380426692263f2fb
Johana <johanarybarova@gmail.com>
2022-07-29 11:58:35 +0200

    feat: add dashboard styles

--- src/features/events/components/EventsList/index.tsx
+++ src/features/events/components/EventsList/index.tsx
@@ -1,25 +1,62 @@
 import type { FC } from 'react'
+import { useState } from 'react'
 
 import { EventCard } from './parts/EventCard'
 import { NavFilter } from './parts/NavFilter'
 import { NavView } from './parts/NavView'
+import { List, Nav } from './styled'
+import { ViewType } from './types'
 
-export const EventsList: FC = () => (
-  <>
-    <nav>
-      <NavFilter onChange={(filterType) => alert(filterType)} />
-      <NavView onChange={(viewType) => alert(viewType)} />
-    </nav>
-    <ul>
-      <li>
-        <EventCard />
-      </li>
-      <li>
-        <EventCard />
-      </li>
-      <li>
-        <EventCard />
-      </li>
-    </ul>
-  </>
-)
+export const EventsList: FC = () => {
+  const [view, setView] = useState<ViewType>(ViewType.GRID)
+  return (
+    <>
+      <Nav>
+        <NavFilter onChange={(filterType) => alert(filterType)} />
+        <NavView activeView={view} onChange={setView} />
+      </Nav>
+
+      <List isView={Boolean(view === ViewType.GRID)}>
+        <li>
+          <EventCard isRow={view === ViewType.LIST} />
+        </li>
+
+        <li>
+          <EventCard isRow={view === ViewType.LIST} />
+        </li>
+
+        <li>
+          <EventCard isRow={view === ViewType.LIST} />
+        </li>
+
+        <li>
+          <EventCard isRow={view === ViewType.LIST} />
+        </li>
+
+        <li>
+          <EventCard isRow={view === ViewType.LIST} />
+        </li>
+
+        <li>
+          <EventCard isRow={view === ViewType.LIST} />
+        </li>
+
+        <li>
+          <EventCard isRow={view === ViewType.LIST} />
+        </li>
+
+        <li>
+          <EventCard isRow={view === ViewType.LIST} />
+        </li>
+
+        <li>
+          <EventCard isRow={view === ViewType.LIST} />
+        </li>
+
+        <li>
+          <EventCard isRow={view === ViewType.LIST} />
+        </li>
+      </List>
+    </>
+  )
+}
--- src/features/events/pages/DashboardPage/index.tsx
+++ src/features/events/pages/DashboardPage/index.tsx
@@ -1,17 +1,21 @@
 import type { NextPage } from 'next'
 
 import { EventsList } from '~/features/events/components/EventsList'
+import { Container } from '~/features/ui/components/Container'
 import { LayoutIn } from '~/features/ui/components/LayoutIn'
+import { ScreenReaderOnly } from '~/features/ui/components/ScreenReaderOnly'
 
-import { CreateKnob } from '../../components/EventsList/parts/Create'
+import { CreateKnob } from '../../components/EventsList/parts/CreateKnob'
 
 export const DashboardPage: NextPage = () => (
   <LayoutIn>
-    <h1>Dashboard</h1>
-    <section>
-      <h2>Events List</h2>
-      <EventsList />
-      <CreateKnob />
-    </section>
+    <Container>
+      <h1 className={ScreenReaderOnly()}>Dashboard</h1>
+      <section>
+        <h2 className={ScreenReaderOnly()}>Events List</h2>
+        <EventsList />
+        <CreateKnob />
+      </section>
+    </Container>
   </LayoutIn>
 )
