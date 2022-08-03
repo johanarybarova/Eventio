d69eaabfdf92c5343d8642aff2ab70060fd7d484
Johana <johanarybarova@gmail.com>
2022-08-03 18:25:08 +0200

    feat: add EyeIcon svg

--- src/features/ui/components/Input/index.tsx
+++ src/features/ui/components/Input/index.tsx
@@ -1,6 +1,7 @@
 import type { FC, InputHTMLAttributes } from 'react'
 import { useState } from 'react'
 
+import { EyeIcon } from './parts/EyeIcon'
 import {
   InputWrapper,
   Label,
@@ -35,7 +36,7 @@ export const Input: FC<Props> = ({ label, name, type, error, ...rest }) => {
             onClick={() => togglePassword(!isPasswordShown)}
             aria-label="Display password text"
           >
-            👁
+            <EyeIcon />
           </PasswordToggle>
         )}
       </Label>
