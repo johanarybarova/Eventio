89a47277edad88d681ea759a73ec8622bdd32c1c
Johana <johanarybarova@gmail.com>
2022-08-04 09:54:45 +0200

    feat: add  PlusIcon svg

--- src/features/events/components/EventsList/parts/CreateKnob/index.tsx
+++ src/features/events/components/EventsList/parts/CreateKnob/index.tsx
@@ -3,10 +3,13 @@ import type { FC } from 'react'
 
 import { Routes } from '~/features/core/constants/routes'
 
+import { PlusIcon } from './parts/PlusIcon'
 import { CreateLink } from './styled'
 
 export const CreateKnob: FC = () => (
   <Link href={Routes.CREATE_EVENT} passHref>
-    <CreateLink aria-label="Create Event">+</CreateLink>
+    <CreateLink aria-label="Create Event">
+      <PlusIcon />
+    </CreateLink>
   </Link>
 )
