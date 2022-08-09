2dc3e776cbd48a67756a2b07f4f63c1fac673050
Johana <johanarybarova@gmail.com>
2022-08-09 15:40:39 +0200

    feat: add simple test get request with Ky and useEffect hook

--- src/features/events/components/EventsList/index.tsx
+++ src/features/events/components/EventsList/index.tsx
@@ -1,5 +1,6 @@
+import ky from 'ky'
 import type { FC } from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import { EventCard } from './parts/EventCard'
 import { NavFilter } from './parts/NavFilter'
@@ -9,26 +10,41 @@ import { ViewType } from './types'
 
 export const EventsList: FC = () => {
   const [view, setView] = useState<ViewType>(ViewType.GRID)
-    const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true)
   const [events, setEvents] = useState([])
-  
-    return (
-      <>
-        <Nav>
-          <NavFilter onChange={(filterType) => alert(filterType)} />
-          <NavView activeView={view} onChange={setView} />
-        </Nav>
-        {loading ? (
-          <div> ...loading </div>
-        ) : (
-          <List isView={Boolean(view === ViewType.GRID)}>
-            {events.map((event) => (
-              <li key={event.id}>
-                <EventCard event={event} isRow={view === ViewType.LIST} />
-              </li>
-            ))}
-          </List>
-        )}
-      </>
-    )
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    ;(async () => {
+      const apiUrl = process.env.NEXT_PUBLIC_API_URL
+      const apiKey = process.env.NEXT_PUBLIC_API_KEY
+
+      const data = await ky
+        .get(`${apiUrl}events`, { headers: { APIKey: apiKey } })
+        .json()
+
+      setEvents(data)
+      setLoading(false)
+    })()
+  }, [])
+
+  return (
+    <>
+      <Nav>
+        <NavFilter onChange={(filterType) => alert(filterType)} />
+        <NavView activeView={view} onChange={setView} />
+      </Nav>
+      {loading ? (
+        <div> ...loading </div>
+      ) : (
+        <List isView={Boolean(view === ViewType.GRID)}>
+          {events.map((event) => (
+            <li key={event.id}>
+              <EventCard event={event} isRow={view === ViewType.LIST} />
+            </li>
+          ))}
+        </List>
+      )}
+    </>
+  )
 }
