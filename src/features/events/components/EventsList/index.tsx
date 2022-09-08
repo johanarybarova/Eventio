5c8cffd6db8fd18e6aedf4df1901e95fb14ad895
Johana <johanarybarova@gmail.com>
2022-09-08 14:43:28 +0200

    refactor: abstract some logic api

--- src/features/events/components/EventsList/index.tsx
+++ src/features/events/components/EventsList/index.tsx
@@ -1,7 +1,5 @@
-import ky from 'ky'
 import type { FC } from 'react'
 import { useState } from 'react'
-import { useQuery } from 'react-query'
 
 import { EventCard } from './parts/EventCard'
 import { NavFilter } from './parts/NavFilter'
@@ -9,20 +7,12 @@ import { NavView } from './parts/NavView'
 import { List, Nav } from './styled'
 import { ViewType } from './types'
 
+import { useEvents } from '../../hooks/useEvents'
+
 export const EventsList: FC = () => {
   const [view, setView] = useState<ViewType>(ViewType.GRID)
-
-  const { data, isLoading } = useQuery('queryevents', async () => {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL
-    const apiKey = process.env.NEXT_PUBLIC_API_KEY
-
-    const data = await ky
-      .get(`${apiUrl}events`, { headers: { APIKey: apiKey } })
-      .json()
-
-    return data
-  })
-
+  const { data, isLoading } = useEvents()
+  
   return (
     <>
       <Nav>
@@ -33,7 +23,7 @@ export const EventsList: FC = () => {
         <div> ...loading </div>
       ) : (
         <List isView={Boolean(view === ViewType.GRID)}>
-          {data.map((event) => (
+          {data?.map((event) => (
             <li key={event.id}>
               <EventCard event={event} isRow={view === ViewType.LIST} />
             </li>
