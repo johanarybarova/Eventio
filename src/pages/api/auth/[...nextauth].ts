925b3d8f18a74074656dec56d48c8b375b7ad06e
johanarybarova <johanarybarova@gmail.com>
2023-02-18 14:15:58 +0100

    fix: err

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,57 +1,58 @@
-import ky from 'ky'
-import NextAuth, { NextAuthOptions } from 'next-auth'
-import CredentialProvider from 'next-auth/providers/credentials'
+import ky from "ky";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialProvider from "next-auth/providers/credentials";
 
 const authOptions: NextAuthOptions = {
   session: {
-    strategy: 'jwt',
+    strategy: "jwt",
   },
   providers: [
     CredentialProvider({
-      type: 'credentials',
+      type: "credentials",
       credentials: {},
-      async authorize(credentials, _req) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      async authorize(credentials, req) {
         const { email, password } = credentials as {
-          email: string
-          password: string
-        }
+          email: string;
+          password: string;
+        };
 
         // Set up the request body
         const body = {
           email,
           password,
-        }
+        };
 
         // Set up the request headers
         const headers = {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
           apiKey: process.env.NEXT_PUBLIC_API_KEY,
-        }
+        };
 
         // Send the POST request to the /auth/native endpoint
         const response = await ky.post(
-          'https://testproject-api-v2.strv.com/auth/native',
+          "https://testproject-api-v2.strv.com/auth/native",
           {
             body: JSON.stringify(body),
             headers,
-          }
-        )
+          },
+        );
 
         // Check the response status to see if the request was successful
         if (response.status !== 200) {
-          return null
+          return null;
         }
 
         // If the request was successful, return the user object
-        const user = await response.json()
-        console.log(user)
-        return user
+        const user = await response.json();
+        console.log(user);
+        return user;
       },
     }),
   ],
   pages: {
-    signIn: '/',
+    signIn: "/",
   },
-}
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
