d5edf2ea88c09446c9deb58e2ad14eda897652f8
johanarybarova <johanarybarova@gmail.com>
2023-02-19 14:42:49 +0100

    refactor: api

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,62 +1,51 @@
-import ky from "ky";
-import type { NextAuthOptions } from "next-auth";
-import NextAuth from "next-auth";
-import CredentialProvider from "next-auth/providers/credentials";
+import type { NextAuthOptions } from 'next-auth'
+import NextAuth from 'next-auth'
+import CredentialProvider from 'next-auth/providers/credentials'
 
-import type { User } from "~/features/events/types";
+import type { User } from '~/features/events/types'
+import { api } from '~/pages/api/client'
 
 const authOptions: NextAuthOptions = {
   session: {
-    strategy: "jwt",
+    strategy: 'jwt',
   },
   providers: [
     CredentialProvider({
-      type: "credentials",
+      type: 'credentials',
       credentials: {},
-  
+      
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       async authorize(credentials, _req) {
         const { email, password } = credentials as {
-          email: string;
-          password: string;
-        };
-
-        // Set up the request body
-        const body = {
-          email,
-          password,
-        };
-
-        // Set up the request headers
-        const headers = {
-          "Content-Type": "application/json",
-          apiKey: process.env.NEXT_PUBLIC_API_KEY,
-        };
-
-        // Send the POST request to the /auth/native endpoint
-        const response = await ky.post(
-          "https://testproject-api-v2.strv.com/auth/native",
-          {
-            body: JSON.stringify(body),
-            headers,
-          },
-        );
-
-        // Check the response status to see if the request was successful
-        if (response.status !== 200) {
-          return null;
+          email: string
+          password: string
         }
 
-        // If the request was successful, return the user object
-        const user: User = await response.json();
-        console.log(user);
-        return user;
+        try {
+          const response = await api.post('auth/native', {
+            json: {
+              email,
+              password,
+            },
+          })
+
+          if (response.status !== 200) {
+            return null
+          }
+
+          const user: User = await response.json()
+          console.log(user)
+          return user
+        } catch (error) {
+          console.log(error)
+          return null
+        }
       },
     }),
   ],
   pages: {
-    signIn: "/",
+    signIn: '/',
   },
-};
+}
 
-export default NextAuth(authOptions);
+export default NextAuth(authOptions)
